51f96d16e80708b7ecbfaba30e5e3d4dbb6c32ee
sahinmemmedzade <shahinim@code.edu.az>
2023-12-29 13:10:57 +0400

    12.29.2023

---
+++ js.js
@@ -0,0 +1,37 @@
+/*var number=455;
+var boolean=false;
+var c;
+while( number>=1){
+var c=number%10;
+if(c==5){
+    boolean=true;
+    console.log("var");
+    break;
+}
+number=number/10;
+}
+if(c!=5){
+console.log("yoxdu")
+}
+
+*/
+/*2 ci
+var number=10000;
+var count=0;
+var a;
+var b;
+var c=number
+
+while( number>=1){
+count++
+number=number/10;
+}
+if(count==5){
+    a=c*18/100;
+    b=a*3/100
+    console.log(b)
+}
+else if(count!=5){
+console.log("5 reqemli deyil")
+}
+*/
